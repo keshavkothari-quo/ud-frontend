950c841ce554b2c61c90ffd222302a8c897b5294
keshavkothari-quo <keshav.kothari@quovantis.com>
2021-01-05 12:35:49 +0530

    frontend user directory

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,60 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from '@/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    register(name,email,password) {
+        return this.http.post<any>(`${config.apiUrl}/api/register`, { name, email, password }).pipe(map(user => {
+            return this.setAuthorization(user);
+        }))
+    }
+
+    login(email, password) {
+        
+        return this.http.post<any>(`${config.apiUrl}/api/login`, { email, password })
+            .pipe(map(user => {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                return this.setAuthorization(user);
+            }));
+    }
+
+    setAuthorization(user){
+        // store user details and jwt token in local storage to keep user logged in between page refreshes
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        localStorage.setItem('Authorization',user.data.token);
+        this.currentUserSubject.next(user);
+        return user;
+    }
+
+    logout() {
+        // remove user from local storage and set current user to null
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+
+    getDashboard(){
+        let authorization = localStorage.getItem("Authorization");
+        const headers= new HttpHeaders()
+                .set('content-type', 'application/json')
+                .set('Authorization',authorization);
+        
+        return this.http.get(`${config.apiUrl}/api/dashboard`, { 'headers': headers });
+    }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@/_models';
+import { AuthenticationService, AlertService } from '@/_services';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    userName;
+    userEmail;
+    userData;
+    loading = false;
+    clicked = false;
+
+    constructor(
+        private authenticationService: AuthenticationService,
+        private alertService: AlertService,
+        private formBuilder: FormBuilder,
+
+    ) {
+        this.currentUser = this.authenticationService.currentUserValue;
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    private loadAllUsers() {
+        this.loading = true;
+        this.authenticationService.getDashboard().pipe(first())
+            .subscribe(data => {
+                this.userData = data['data'];
+            },
+            error => {
+                    this.alertService.error(error);
+                    
+                    this.loading = false;
+            });
+    }   
+}
